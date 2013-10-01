7ce2dfcb4d690de96b061206b82e5d0cb9a66960
Ralph Bolo <ralph.bolo@gmail.com>
2013-10-01 15:41:20 -0400

    tests for login and logout

---
+++ spec/support/sessions_support.rb
@@ -0,0 +1,13 @@
+module SessionSupport
+	def setup_signed_in_user
+		pass = "this=is-a-password"
+		user = FactoryGirl.create :user, password: pass
+		visit '/session/new'
+
+		fill_in "email", with: user.email
+		fill_in "password", with: pass
+		click_button "Login"
+
+		user
+	end
+end
